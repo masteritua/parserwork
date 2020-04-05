bc0f0e00fdc246b920e26285443cdf1dfaae6cfe
Aleksey Shevchenko <masteritua@gmail.com>
2020-04-05 08:52:05 +0300

    fix-parsedata

--- src/parse/utils.py
+++ src/parse/utils.py
@@ -1,22 +1,18 @@
 import random
 from time import sleep
-from parse.models import *
 import json
 
+
 def save_json(data):
-
-    saveFile = open('data.json', 'w')
-    saveFile.write(json.dumps(data))
-    saveFile.close()
-
-
-
+    with open('data.json', 'w') as file:
+        file.write(json.dumps(data))
 
 
 def save_info(array: list) -> None:
     with open('../parser/save/workua2.txt', 'a') as file:
         for line in array:
             file.write(' | '.join(line) + '\n')
+
 
 def random_sleep():
     sleep(random.randint(1, 4))
